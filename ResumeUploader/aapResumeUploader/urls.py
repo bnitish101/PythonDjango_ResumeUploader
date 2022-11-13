2136afcf43758cf68d8be1e7f1821f6226d56ef0
Nitish <bnitish101@gmail.com>
2022-11-14 00:33:01 +0530

    fixed media url in main url.py

--- ResumeUploader/aapResumeUploader/urls.py
+++ ResumeUploader/aapResumeUploader/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 
-from django.conf import settings
-from django.conf.urls.static import static
 
 from . import views
 urlpatterns = [
     path('/', views.ResumeCreateView.as_view(), name='resume'),
     path('/candidate_detail/<slug:pk>', views.CandidateDetailView.as_view(), name='candidate_detail'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
